778d28afef7910084d1046a1305cd2951be7958d
Eric Alsheimer <ealsheimer@gmail.com>
2017-10-14 10:22:43 -0400

    add README, objectify solution

---
+++ DeadlockSimulation/README.md
@@ -0,0 +1,8 @@
+# Deadlock Simulator
+
+This project simulates a multithread deadlock situation using String objects for locked resources
+
+## Interview Question
+"When two threads are using the same resources, they have a chance to enter a deadlocked state where both threads
+are holding all the necessary resources but neither thread has all their resources they require.  Write a program
+to demonstrate this situation in runtime"
--- DeadlockSimulation/src/com/technoangel/interview/Deadlock.java
+++ DeadlockSimulation/src/com/technoangel/interview/Deadlock.java
@@ -2,35 +2,53 @@ package com.technoangel.interview;
 
 public class Deadlock {
 
-	String str1 = "Java";
-	String str2 = "Unix";
-	
-	Runnable r1 = () -> {
-		while(true) {
-			synchronized(str1) {
-				synchronized(str2) {
-					System.out.println(str1 + str2 + Thread.currentThread().getName());
+	public String resource1;
+	public String resource2;
+
+	public Runnable r1;
+	public Runnable r2;
+	public Thread t1;
+	public Thread t2;
+
+	public void printStatus(String resource1, String resource2) {
+		System.out.println("Using both " + resource1 + " and " + resource2 + " :: " + Thread.currentThread().getName());
+	}
+
+	public void startRace() {
+		this.t1.start();
+		this.t2.start();
+	}
+
+	public Deadlock() {
+		resource1 = "Resource 1";
+		resource2 = "Resource 2";
+
+		r1 = () -> {
+			while (true) {
+				synchronized (resource1) {
+					synchronized (resource2) {
+						printStatus(resource1, resource2);
+					}
 				}
 			}
-		}
-	};
-	
-	Runnable r2 = () -> {
-		while(true) {
-			synchronized(str1) {
-				synchronized(str2) {
-					System.out.println(str1 + str2 + Thread.currentThread().getName());
+		};
+
+		r2 = () -> {
+			while (true) {
+				synchronized (resource2) {
+					synchronized (resource1) {
+						printStatus(resource1, resource2);
+					}
 				}
 			}
-		}
-	};
-	
-	Thread t1 = new Thread(r1, "Thread Alpha");
-	Thread t2 = new Thread(r2, "Thread Beta");
-	
+		};
+
+		t1 = new Thread(r1, "Thread Alpha");
+		t2 = new Thread(r2, "Thread Beta");
+	}
+
 	public static void main(String[] args) {
 		Deadlock myDeadlock = new Deadlock();
-		myDeadlock.t2.start();
-		myDeadlock.t1.start();
+		myDeadlock.startRace();
 	}
 }
